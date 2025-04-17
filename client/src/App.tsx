8bfefb9b10d9923e6ed1f33f0d0e215be6c2cc1c
dbay <skynetminer4@gmail.com>
2025-04-17 09:39:37 -0400

    Navbar component created

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,8 +3,6 @@ import Navbar from "./components/Navbar";
 function App() {
   return (
     <div>
-      <h1>Hello!</h1>
-      <p>This is my app.</p>
       <Navbar />
     </div>
   );
--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -1,9 +1,39 @@
+import "./navbar.css";
+import React, { useState } from "react";
+import { FiMenu, FiSun } from "react-icons/fi";
+
 function Navbar() {
+  const [nav, setNav] = useState(false);
+  const handleNav = () => {
+    setNav(!nav);
+  };
+
+  const [mode, setMode] = useState(false);
+  const handleMode = () => {
+    setMode(!mode);
+  };
+
   return (
-    <div>
-      <h1>This is the navbar!</h1>
+    <div className="navbar-class ">
+      <div onClick={handleNav} className="p-2 m-5">
+        <FiMenu size={30} />
+      </div>
+      <div className={!nav ? "md:hidden" : "fixed left-0"}>
+        <ul className="p-2 m-5">
+          <li>home.</li>
+          <li>about.</li>
+          <li>overview.</li>
+          <li>projects.</li>
+          <li>contact.</li>
+        </ul>
+        <div className="p-2 m-5 absolute bottom-[-50%]">
+          <FiSun size={30} />
+        </div>
+      </div>
     </div>
   );
 }
+
+// Work on mode button. Setup md and lg screen
 
 export default Navbar;
